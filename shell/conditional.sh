2b2ecf6e90cf1b346f673c2c50e254dbecf41da2
Kazuma <21kazuma21@gmail.com>
2024-10-30 22:30:05 +0900

    shell script / 条件分岐

--- shell/conditional.sh
+++ shell/conditional.sh
@@ -1,6 +1,6 @@
 # 数値の比較
-number1=10
-number2=20
+number_one=10
+number_two=20
 
 if [ $number1 -eq $number2 ]; then
   echo "num1 と num2 は等しい"
@@ -8,16 +8,15 @@ else
   echo "値は等しくありません"
 fi
 
-
 # 文字列の比較
 word="こんにちは"
 
-if [ $word = "こんにちは" ];  then
+if [ $word = "こんにちは" ]; then
   echo "${word}"
   echo "こんにちは!!!"
-elif [ $word = "こんばんは" ];  then
+elif [ $word = "こんばんは" ]; then
   echo "${word}"
   echo "こんばんは!!!"
 else
-  echo "もう寝ようぜ~"
+  echo "もう夜だよ~"
 fi
