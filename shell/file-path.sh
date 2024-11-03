f5dcd3c68b4fae7795defe3fa2ccd1d34f5eb542
Kazuma <21kazuma21@gmail.com>
2024-11-03 22:00:03 +0900

    shell script / path

---
+++ shell/file-path.sh
@@ -0,0 +1,14 @@
+# 実行コマンド: bash file-path.sh /path/to/file.txt
+$file_path=$1
+
+if [[ "$file_path" = /* ]]; then
+  echo "絶対パスです : $file_path"
+else
+  echo "相対パスです : $file_path"
+fi
+
+if [ -e "$file_path" ]; then
+  echo "ファイルが存在します"
+else
+  echo "ファイルが存在しません"
+fi
