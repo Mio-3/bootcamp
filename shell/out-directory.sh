e629ac59346832db51f057d8d752b95c09912a1e
Kazuma <21kazuma21@gmail.com>
2024-11-04 23:46:46 +0900

    shell script / out ディレクトリ

---
+++ shell/out-directory.sh
@@ -0,0 +1,16 @@
+number_direction=$1
+
+mkdir -p out
+
+for i in $(seq 1 $number_direction);
+do
+  directory_name="out/dir-$i"
+  mkdir -p "$directory_name"
+
+  for j in $(seq 1 $i);
+  do
+    touch "$directory_name/file-$j"
+  done
+done
+
+echo "ディレクトリとファイルの作成が完了しました"
