8810cd8ee21097a3fa9beefacce9b15117a9ac53
Kazuma <21kazuma21@gmail.com>
2024-11-04 14:58:07 +0900

    実行方法を追加しました。

--- shell/script-a.sh
+++ shell/script-a.sh
@@ -5,6 +5,12 @@ bash ./script-b.sh
 
 # source ./script-b.sh
 
+# csh ./script-b.sh
+
+# tcsh ./script-b.sh
+
 # source ./script-b.sh でも実行可能
 # sourceはスクリプトを現在のシェルで実行する
 # bashは新しいシェルで実行する
+# cshはCシェルでスクリプトを実行する。
+# tcshはCシェルの改良版で、コマンドライン編集や履歴機能などの追加機能を持つ。
